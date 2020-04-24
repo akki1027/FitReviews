c87dd19b51793e582ad4ecad2b5ef3a0d65081a8
akki1027 <akki10271995@gmail.com>
2020-04-24 14:53:22 +0000

    Able to sign-up/sign-in with name and Change path after sign-up/sign-in/sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+	before_action :authenticate_user!, except: [:top, :fr_top, :about]
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+	end
+	# 会員登録後に移動するviewの指定
+	def after_sign_up_path_for(resource)
+    	fr_top_path
+    end
+
+    # ログイン後に移動するviewの指定
+    def after_sign_in_path_for(resource)
+    	fr_top_path
+    end
+	# ログアウト後に移動するviewの指定
+    def after_sign_out_path_for(resource)
+    	fr_top_path
+    end
 end
