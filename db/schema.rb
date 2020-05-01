1afc3882c3fa26522c1d506d78797ab6caa962a5
akki1027 <akki10271995@gmail.com>
2020-05-01 06:01:04 +0000

    Add model and controller for bookmark

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_032742) do
+ActiveRecord::Schema.define(version: 2020_05_01_054155) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
