720fa65aa4fb3a570d4e06f4872014b369b8ee22
akki1027 <akki10271995@gmail.com>
2020-05-01 05:24:58 +0000

    Like reviews with AJAX

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,17 @@
 class LikesController < ApplicationController
 	def create
-		review = Review.find(params[:review_id])
-		like = current_user.likes.new(review_id: review.id)
+		@review = Review.find(params[:review_id])
+		like = current_user.likes.new(review_id: @review.id)
 		like.save
-		redirect_to request.referrer || root_path
+		# AJAX
+		# redirect_to request.referrer || root_path
 	end
 
 	def destroy
-		review = Review.find(params[:review_id])
-		like = current_user.likes.find_by(review_id: review.id)
+		@review = Review.find(params[:review_id])
+		like = current_user.likes.find_by(review_id: @review.id)
 		like.destroy
-		redirect_to request.referrer || root_path
+		# AJAX
+		# redirect_to request.referrer || root_path
 	end
 end
