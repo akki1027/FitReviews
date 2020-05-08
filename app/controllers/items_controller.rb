f327b0288e07c8dcd2df15b1b06bbb368d8663d3
akki1027 <akki10271995@gmail.com>
2020-05-08 14:08:36 +0000

    Add ranking page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def search
   	if params[:keyword]
-  		@items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
     end
   end
 
@@ -9,5 +9,6 @@ class ItemsController < ApplicationController
   end
 
   def ranking
+  	@rankings = Review.group(:item_id).limit(5).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
   end
 end
