1ad3b85211aac0954f3662918b23cbe1c27a55a9
akki1027 <akki10271995@gmail.com>
2020-05-08 14:10:32 +0000

    Add some changes to FR top page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,12 @@ class HomesController < ApplicationController
   end
 
   def fr_top
+  	@rankings = Review.group(:item_id).limit(3).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
+  	@reviews = Review.all.page(params[:page]).reverse_order
+    @items = []
+    @reviews.each do |review|
+      @items << RakutenWebService::Ichiba::Item.search(itemCode: review.item.rakuten_item_id)
+    end
   end
 
   def about
