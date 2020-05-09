8a79700adc18fec720f07942e32cb5a416ce6c5e
akki1027 <akki10271995@gmail.com>
2020-05-09 13:55:01 +0000

    Change layout for MyPage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @reviews = Review.where(user_id: current_user.id).page(params[:page]).reverse_order
-    @items = []
-    @reviews.each do |review|
-      @items << RakutenWebService::Ichiba::Item.search(itemCode: review.item.rakuten_item_id)
-    end
     @bookmarks = Bookmark.where(user_id: current_user.id).page(params[:page]).per(4).reverse_order
     @bookmarked_items = []
     @bookmarks.each do |bookmark|
       @bookmarked_items << RakutenWebService::Ichiba::Item.search(itemCode: bookmark.item.rakuten_item_id)
+    end
+    @reviews = Review.where(user_id: current_user.id).page(params[:page]).reverse_order
+    @items = []
+    @reviews.each do |review|
+      @items << RakutenWebService::Ichiba::Item.search(itemCode: review.item.rakuten_item_id)
     end
   end
 
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
     end
   end
 
-  protected
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :introduction, :profile_photo, :background_photo, :is_deleted)
   end
