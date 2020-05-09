bf4bd5f9b7d245ab0fee170d7b827dd8ee1f8d94
akki1027 <akki10271995@gmail.com>
2020-05-09 12:53:13 +0000

    Improve ranking page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,11 @@ class ItemsController < ApplicationController
   end
 
   def ranking
-  	@rankings = Review.group(:item_id).limit(5).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
+    if params[:genreId]
+      @rankings = Review.joins(:item).where("items.rakuten_genre_id = ?", params[:genreId]).group(:item_id).limit(5).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
+    else
+      @rankings = Review.joins(:item).where("items.rakuten_genre_id = ?", 567603).group(:item_id).limit(5).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
+    end
+    # @rankings = Review.group(:item_id).limit(5).select("reviews_item_id, average_rate").order('average_rate desc').average(:rate)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,18 +5,13 @@ class ReviewsController < ApplicationController
 
   def create
     @items = RakutenWebService::Ichiba::Item.search(itemCode: params[:item_code])
-    # アイテムの保存
     # transactionを使うことで、itemが保存され、尚且つreviewも保存されないと処理が完了しない
     result = ActiveRecord::Base.transaction do
       @item = Item.find_by(item_params)
       @review = current_user.reviews.create(review_params(@item))
       true
     end
-    if result
-      # flash[:notice] = "レビューを作成しました。"
-      # 非同期通信のためコメントアウト
-      # redirect_to reviews_new_path(@items.first["itemCode"])
-    else
+    if result != true
       render 'new'
     end
   end
@@ -49,23 +44,38 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     item = Item.find_by(id: @review.item_id)
     @review.destroy
-    # newページのURLには、楽天の商品Codeを含みたいので、rakuten_item_idに返す
-    # 非同期通信のためコメントアウト
+    # 非同期通信のため以下をコメントアウト
+    # newページのURLには、楽天のitemCodeを含みたいので、rakuten_item_idに返す
     # redirect_to reviews_new_path(item.rakuten_item_id)
   end
 
   def search
     if params[:keyword]
-      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+      @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], genreId: params[:genreId])
     else
-      @items = RakutenWebService::Ichiba::Item.search(keyword: "筋トレ")
+      @items = RakutenWebService::Ichiba::Item.search(keyword: "プロテイン", genreId: 567603)
     end
   end
 
   def save_item
-    # レビューを作成する画面に移動する時にその楽天商品をデータベースに保存する
+    # new画面に移動する時にその楽天商品をデータベースに保存する
     @items = RakutenWebService::Ichiba::Item.search(itemCode: params[:itemCode])
-    save_item = Item.find_or_create_by(item_params)
+    genre_id = @items.first['genreId']
+    genre_ids = [567603, 566807, 563727, 201865]
+    index = genre_ids.index(genre_id)
+    # ４つのジャンルIDに必ず紐付けたい。現状、子供のジャンルIDのデータも検索できてしまうため、商品に紐づくジャンルIDが４つのジャンルに属さない可能性がある。
+    # そのため、最も近しい親ジャンルを商品に紐付ける。なので、楽天の持つ商品とジャンルの紐付きと必ずしも一致しない仕様。
+    if index.nil?
+      parent_genres = RakutenWebService::Ichiba::Genre[genre_id].parents
+      parent_genres.reverse_each do |parent|
+        parent_index = genre_ids.index(parent.params["genreId"])
+        if parent_index.present?
+          genre_id = genre_ids[parent_index]
+          break
+        end
+      end
+    end
+    Item.find_or_create_by({ name: @items.first['itemName'], rakuten_item_id: @items.first['itemCode'], rakuten_genre_id: genre_id })
     redirect_to reviews_new_path(@items.first["itemCode"])
   end
 
