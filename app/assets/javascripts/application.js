05cba577f06d93370b16b6c9d0c1c5bd0259fc96
akki1027 <akki10271995@gmail.com>
2020-07-09 03:22:15 +0000

    Fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,50 +17,50 @@
 //= require bootstrap-sprockets
 //= require jquery.raty.js
 
-$(function() {
-    $('#show-nav').click(function() {
+$(function () {
+    $('#show-nav').click(function () {
         $('#top-nav-list').fadeIn();
     });
-    $('#hide-nav').click(function() {
-        $('#top-nav-list').fadeOut()
+    $('#hide-nav').click(function () {
+        $('#top-nav-list').fadeOut();
     })
- });
-
-$(function() {
-	$('#show-nav').click(function() {
-		$('#nav-list').fadeIn();
-	});
-	$('#hide-nav').click(function() {
-		$('#nav-list').fadeOut()
-	})
- });
-
-$(function() {
-	$('#show-form').click(function() {
-    	$("#review-form").toggle();
-	});
 });
 
-$(function() {
-	$('.form').on('change', '.file', function(event) {
-    	var file = event.target.files[0];
+$(function () {
+    $('#show-nav').click(function () {
+        $('#nav-list').fadeIn();
+    });
+    $('#hide-nav').click(function () {
+        $('#nav-list').fadeOut();
+    })
+});
+
+$(function () {
+    $('#show-form').click(function () {
+        $("#review-form").toggle();
+    });
+});
+
+$(function () {
+    $('.form').on('change', '.file', function (event) {
+        var file = event.target.files[0];
         var reader = new FileReader();
 
-    if(file.type.indexOf("image") < 0){
-      return false;
-    }
+        if (file.type.indexOf("image") < 0) {
+            return false;
+        }
 
-    reader.onload = (function(file) {
-    	return function(event) {
-        $(".preview").empty();
+        reader.onload = (function (file) {
+            return function (event) {
+                $(".preview").empty();
 
-        $(".preview").append($('<img>').attr({
-			src: event.target.result,
-			width: "200px"
-            }));
-    	};
-    })(file);
+                $(".preview").append($('<img>').attr({
+                    src: event.target.result,
+                    width: "200px"
+                }));
+            };
+        })(file);
 
-    reader.readAsDataURL(file);
-	});
+        reader.readAsDataURL(file);
+    });
 });
