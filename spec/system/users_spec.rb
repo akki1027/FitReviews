4d35933dd27a99caf159ff0f6f27b9d38331127d
akki1027 <akki10271995@gmail.com>
2020-08-10 22:49:04 +0900

    Rspec: add more test items to user system file

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,7 +5,6 @@ describe '新規登録のテスト' do
     before do
       visit new_user_registration_path
     end
-    # render_views
     it '新規登録に成功する'do
       fill_in 'user[name]', with: Faker::Internet.username(specifier: 5)
       fill_in 'user[email]', with: Faker::Internet.email
@@ -14,6 +13,27 @@ describe '新規登録のテスト' do
       click_on '新規登録'
 
       expect(page).to have_content 'アカウント登録が完了しました'
+    end
+  end
+  describe 'ユーザーのテスト' do
+    let(:user) { create(:user) }
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+    end
+    context 'マイページのテスト' do
+      before do
+        visit user_path(user)
+      end
+      it '名前が表示される' do
+        expect(page).to have_content(user.name)
+      end
+      it '編集リンクが表示される' do
+        visit user_path(user)
+        expect(page).to have_link '', href: edit_user_path(user)
+      end
     end
   end
 end
