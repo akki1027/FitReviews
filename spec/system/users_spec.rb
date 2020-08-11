82c4bc844342cb2a588ab0bf0b2f5158c5e9dd27
akki1027 <akki10271995@gmail.com>
2020-08-11 18:18:12 +0900

    Rspec: add more test items to user system file

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -30,9 +30,58 @@ describe '新規登録のテスト' do
       it '名前が表示される' do
         expect(page).to have_content(user.name)
       end
+      it '自己紹介文が表示される' do
+        expect(page).to have_content(user.introduction)
+      end
       it '編集リンクが表示される' do
         visit user_path(user)
         expect(page).to have_link '', href: edit_user_path(user)
+      end
+      it '最近書いたレビューと表示される' do
+        expect(page).to have_content('最近書いたレビュー')
+      end
+      it 'ブックマークした商品と表示される' do
+        expect(page).to have_content('ブックマークした商品')
+      end
+    end
+    before do
+      visit edit_user_path(user)
+    end
+    context 'ユーザー編集画面の表示テスト' do
+      it 'プロフィール画像を追加すると表示される' do
+        expect(page).to have_content('プロフィール画像を追加する')
+      end
+      it '画像編集フォームが表示される' do
+        expect(page).to have_field 'user[profile_photo]'
+      end
+      it 'ユーザーネームと表示される' do
+        expect(page).to have_content('ユーザーネーム')
+      end
+      it '名前編集フォームに自分の名前が表示される' do
+        expect(page).to have_field 'user[name]', with: user.name
+      end
+      it '自己紹介と表示される' do
+        expect(page).to have_content('自己紹介')
+      end
+      it '自己紹介編集フォームに自分の自己紹介が表示される' do
+        expect(page).to have_field 'user[introduction]', with: user.introduction
+      end
+      it 'メールアドレスと表示される' do
+        expect(page).to have_content('メールアドレス')
+      end
+      it 'メールアドレス編集フォームに自分の自己紹介が表示される' do
+        expect(page).to have_field 'user[email]', with: user.email
+      end
+      it '編集に成功する' do
+        click_button '変更を保存する'
+        expect(page).to have_content 'プロフィール情報を編集しました。'
+        expect(current_path).to eq('/users/' + user.id.to_s)
+      end
+      it '編集に失敗する' do
+        fill_in 'user[name]', with: ''
+        click_button '変更を保存する'
+        expect(page).to have_content '入力してください'
+        expect(current_path).to eq('/users/' + user.id.to_s)
       end
     end
   end
